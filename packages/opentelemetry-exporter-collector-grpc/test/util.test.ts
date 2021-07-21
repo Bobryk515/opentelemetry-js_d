Bobryk515/opentelemetry-js_d

commit 0a015879d486e2eb70d4c2fa756256cfabb345f4

chore: url validation & README to prevent gRPC footguns. (#2130)
* extend README and url validation to avoid footguns.

* deal with grpc(s) while here

* fix tests by moving serverAddress checks earlier

* un-private so util can access

* Update packages/opentelemetry-exporter-collector-grpc/src/CollectorExporterNodeBase.ts

Co-authored-by: Gerhard Stöbich <deb2001-github@yahoo.de>

* s#grpc#@grpc/grpc-js#

* address review feedback.

* fix lint and nullcheck

* I hate prettier --fix

* address review feedback.

* my prettier config has diverged :(

* address lint & review feedback.

Co-authored-by: Gerhard Stöbich <deb2001-github@yahoo.de>

packages/opentelemetry-exporter-collector-grpc/src/util.ts
@@ -21,6 +21,7 @@ import { globalErrorHandler } from '@opentelemetry/core';
 import { collectorTypes } from '@opentelemetry/exporter-collector';
 import * as path from 'path';
 import { CollectorExporterNodeBase } from './CollectorExporterNodeBase';
+import { URL } from 'url';
 import {
   CollectorExporterConfigNode,
   GRPCQueueItem,
@@ -32,7 +33,6 @@ export function onInit<ExportItem, ServiceRequest>(
   config: CollectorExporterConfigNode
 ): void {
   collector.grpcQueue = [];
-  const serverAddress = removeProtocol(collector.url);
   const credentials: grpc.ChannelCredentials =
     config.credentials || grpc.credentials.createInsecure();
 
@@ -52,12 +52,12 @@ export function onInit<ExportItem, ServiceRequest>(
 
       if (collector.getServiceClientType() === ServiceClientType.SPANS) {
         collector.serviceClient = new packageObject.opentelemetry.proto.collector.trace.v1.TraceService(
-          serverAddress,
+          collector.serverAddress,
           credentials
         );
       } else {
         collector.serviceClient = new packageObject.opentelemetry.proto.collector.metrics.v1.MetricsService(
-          serverAddress,
+          collector.serverAddress,
           credentials
         );
       }
@@ -105,6 +105,17 @@ export function send<ExportItem, ServiceRequest>(
   }
 }
 
-function removeProtocol(url: string): string {
-  return url.replace(/^https?:\/\//, '');
+export function validateAndNormalizeUrl(url: string): string {
+  const target = new URL(url);
+  if (target.pathname !== '/') {
+    diag.warn(
+      'URL path should not be set when using grpc, the path part of the URL will be ignored.'
+    );
+  }
+  if (target.protocol !== '' && !target.protocol?.match(/(http|grpc)s?/)) {
+    diag.warn(
+      'URL protocol should be http(s):// or grpc(s)://. Using grpc://.'
+    );
+  }
+  return target.host;
 }

packages/opentelemetry-exporter-collector-grpc/src/CollectorExporterNodeBase.ts
@@ -26,6 +26,7 @@ import {
   ServiceClientType,
 } from './types';
 import { ServiceClient } from './types';
+import { validateAndNormalizeUrl } from './util';
 
 /**
  * Collector Metric Exporter abstract base class
@@ -41,6 +42,7 @@ export abstract class CollectorExporterNodeBase<
   grpcQueue: GRPCQueueItem<ExportItem>[] = [];
   metadata?: Metadata;
   serviceClient?: ServiceClient = undefined;
+  serverAddress: string;
   private _send!: Function;
 
   constructor(config: CollectorExporterConfigNode = {}) {
@@ -48,6 +50,8 @@ export abstract class CollectorExporterNodeBase<
     if (config.headers) {
       diag.warn('Headers cannot be set when using grpc');
     }
+
+    this.serverAddress = validateAndNormalizeUrl(this.url);
     this.metadata = config.metadata;
   }
   private _sendPromise(

packages/opentelemetry-exporter-collector-grpc/test/CollectorMetricExporter.test.ts
@@ -129,7 +129,7 @@ const testCollectorMetricExporter = (params: TestParams) =>
           )
         : undefined;
       collectorExporter = new CollectorMetricExporter({
-        url: address,
+        url: 'grpcs://' + address,
         credentials,
         serviceName: 'basic-service',
         metadata: params.metadata,
@@ -166,7 +166,7 @@ const testCollectorMetricExporter = (params: TestParams) =>
 
     describe('instance', () => {
       it('should warn about headers', () => {
-        // Need to stub/spy on the underlying logger as the "diag" instance is global
+        // Need to stub/spy on the underlying logger as the 'diag' instance is global
         const spyLoggerWarn = sinon.stub(diag, 'warn');
         collectorExporter = new CollectorMetricExporter({
           serviceName: 'basic-service',
@@ -177,6 +177,18 @@ const testCollectorMetricExporter = (params: TestParams) =>
         });
         const args = spyLoggerWarn.args[0];
         assert.strictEqual(args[0], 'Headers cannot be set when using grpc');
+      });
+      it('should warn about path in url', () => {
+        const spyLoggerWarn = sinon.stub(diag, 'warn');
+        collectorExporter = new CollectorMetricExporter({
+          serviceName: 'basic-service',
+          url: address + '/v1/metrics',
+        });
+        const args = spyLoggerWarn.args[0];
+        assert.strictEqual(
+          args[0],
+          'URL path should not be set when using grpc, the path part of the URL will be ignored.'
+        );
       });
     });
 

packages/opentelemetry-exporter-collector-grpc/test/CollectorTraceExporter.test.ts
@@ -125,7 +125,7 @@ const testCollectorExporter = (params: TestParams) =>
         : undefined;
       collectorExporter = new CollectorTraceExporter({
         serviceName: 'basic-service',
-        url: address,
+        url: 'grpcs://' + address,
         credentials,
         metadata: params.metadata,
       });
@@ -143,7 +143,7 @@ const testCollectorExporter = (params: TestParams) =>
 
     describe('instance', () => {
       it('should warn about headers when using grpc', () => {
-        // Need to stub/spy on the underlying logger as the "diag" instance is global
+        // Need to stub/spy on the underlying logger as the 'diag' instance is global
         const spyLoggerWarn = sinon.stub(diag, 'warn');
         collectorExporter = new CollectorTraceExporter({
           serviceName: 'basic-service',
@@ -154,6 +154,18 @@ const testCollectorExporter = (params: TestParams) =>
         });
         const args = spyLoggerWarn.args[0];
         assert.strictEqual(args[0], 'Headers cannot be set when using grpc');
+      });
+      it('should warn about path in url', () => {
+        const spyLoggerWarn = sinon.stub(diag, 'warn');
+        collectorExporter = new CollectorTraceExporter({
+          serviceName: 'basic-service',
+          url: address + '/v1/trace',
+        });
+        const args = spyLoggerWarn.args[0];
+        assert.strictEqual(
+          args[0],
+          'URL path should not be set when using grpc, the path part of the URL will be ignored.'
+        );
       });
     });
 

commit 268359e8961d933fe060683bd561579fcaf02b69

fix(@opentelemetry/exporter-collector-grpc) regression from #2130 when host specified without protocol (#2322)
* fix regression from #2130 when host specified without protocol

* Update util.test.ts

* Apply suggestions from code review

Co-authored-by: Naseem <naseemkullah@gmail.com>

* revert package.json changes

* Update util.ts

* add tests as per @msnev request

* Update packages/opentelemetry-exporter-collector-grpc/src/util.ts

Co-authored-by: Naseem <naseemkullah@gmail.com>

Co-authored-by: Naseem <naseemkullah@gmail.com>

packages/opentelemetry-exporter-collector-grpc/src/util.ts
@@ -108,8 +108,12 @@ export function send<ExportItem, ServiceRequest>(
 }
 
 export function validateAndNormalizeUrl(url: string): string {
+  const hasProtocol = url.match(/^([\w]{1,8}):\/\//);
+  if (!hasProtocol) {
+    url = `https://${url}`;
+  }
   const target = new URL(url);
-  if (target.pathname !== '/') {
+  if (target.pathname && target.pathname !== '/') {
     diag.warn(
       'URL path should not be set when using grpc, the path part of the URL will be ignored.'
     );

packages/opentelemetry-exporter-collector-grpc/test/util.test.ts
@@ -1,0 +1,81 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as sinon from 'sinon';
+import * as assert from 'assert';
+
+import { diag } from '@opentelemetry/api';
+import { validateAndNormalizeUrl } from '../src/util';
+
+// Tests added to detect breakage released in #2130
+describe('validateAndNormalizeUrl()', () => {
+  const tests = [
+    {
+      name: 'bare hostname should return same value',
+      input: 'api.datacat.io',
+      expected: 'api.datacat.io',
+    },
+    {
+      name: 'host:port should return same value',
+      input: 'api.datacat.io:1234',
+      expected: 'api.datacat.io:1234',
+    },
+    {
+      name: 'grpc://host:port should trim off protocol',
+      input: 'grpc://api.datacat.io:1234',
+      expected: 'api.datacat.io:1234',
+    },
+    {
+      name: 'bad protocol should warn but return host:port',
+      input: 'badproto://api.datacat.io:1234',
+      expected: 'api.datacat.io:1234',
+      warn: 'URL protocol should be http(s):// or grpc(s)://. Using grpc://.',
+    },
+    {
+      name: 'path on end of url should warn but return host:port',
+      input: 'grpc://api.datacat.io:1234/a/b/c',
+      expected: 'api.datacat.io:1234',
+      warn: 'URL path should not be set when using grpc, the path part of the URL will be ignored.',
+    },
+    {
+      name: ':// in path should not be used for protocol even if protocol not specified',
+      input: 'api.datacat.io/a/b://c',
+      expected: 'api.datacat.io',
+      warn: 'URL path should not be set when using grpc, the path part of the URL will be ignored.',
+    },
+    {
+      name: ':// in path is valid when a protocol is specified',
+      input: 'grpc://api.datacat.io/a/b://c',
+      expected: 'api.datacat.io',
+      warn: 'URL path should not be set when using grpc, the path part of the URL will be ignored.',
+    },
+  ];
+  tests.forEach(test => {
+    it(test.name, () => {
+      const diagWarn = sinon.stub(diag, 'warn');
+      try {
+        assert.strictEqual(validateAndNormalizeUrl(test.input), (test.expected));
+        if (test.warn) {
+          sinon.assert.calledWith(diagWarn, test.warn);
+        } else {
+          sinon.assert.notCalled(diagWarn);
+        }
+      } finally {
+        diagWarn.restore();
+      }
+    });
+  });
+});